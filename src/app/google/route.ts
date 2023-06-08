013ed81df009c29b2290c4d4ea4bffc5b21d72c8
Kai Feng (mini) <kaifeng_toh@mymail.sutd.edu.sg>
2023-06-08 23:24:00 +0800

    base template + able to call token api

---
+++ src/app/google/route.ts
@@ -0,0 +1,45 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import { preProcessFile } from "typescript";
+
+interface GoogleTokenResult {
+	access_token: string;
+	expires_in: Number;
+	refresh_token: string;
+	scope: string;
+	id_token: string;
+}
+
+const getToken = async (code: string): Promise<GoogleTokenResult> => {
+	const url = "https://oauth2.googleapis.com/token";
+	const params = new URLSearchParams({
+		client_id: process.env.GOOGLE_CLIENT_ID as string,
+		client_secret: process.env.GOOGLE_CLIENT_SECRET as string,
+		code,
+		grant_type: "authorization_code",
+		redirect_uri: `${process.env.BASE_URL}/google`,
+	});
+
+	return await fetch(`${url}?${params}`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+	})
+		.then((res) => res.json())
+		.catch((e) => console.log(e));
+};
+
+export async function GET(request: Request) {
+	const requestUrl = new URL(request.url);
+	const searchParams = new URLSearchParams(requestUrl.search);
+	const code = searchParams.get("code") as string;
+
+	const googleTokenResult = await getToken(code);
+	console.log(googleTokenResult);
+	const { access_token, expires_in, refresh_token } = googleTokenResult;
+	return new Response("Hello, Next.js!", {
+		status: 200,
+		headers: { referer: code },
+	});
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import googleURL from './auth/googleURL'
+
+export default function Home() {
+  const loginURL = googleURL();
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="text-red-500">{process.env.GOOGLE_CLIENT_ID}</div>
+      <a className="text-red-500" href={loginURL}>Login</a>
+    </main>
+  )
+}
